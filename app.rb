8a8ec9884ba975f9a2a86453f8f8b79e20640568
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-09 17:52:29 +0000

    Added a separate form

--- app.rb
+++ app.rb
@@ -19,8 +19,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+get '/form' do
+  erb(:form)
+end
+
+post '/named-cat' do
   p params
-  @names = params[:name]
+  @name = params[:name]
   erb(:index)
 end
